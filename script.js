4074ce3c1f87afc877ba39dcc34a07f80aaaad80
Sinan Soylu <bysoylu89@gmail.com>
2025-01-19 21:50:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ const ctx = document.getElementById('tokenomics-chart').getContext('2d');
 new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ['Community', 'Staking Rewards', 'Burn Mechanism', 'Development', 'Others'],
+    labels: ['Community', 'Staking Rewards', 'Burn Mechanism', 'Others'],
     datasets: [{
-      data: [20, 10, 10, 10, 50],
-      backgroundColor: ['#ff0000', '#00ff00', '#0000ff', '#ffa500', '#f4f4f4'],
+      data: [20, 10, 10, 60],
+      backgroundColor: ['#ff0000', '#00ff00', '#0000ff', '#f4f4f4'],
     }]
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
   }
 });
